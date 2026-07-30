9f5f7a2d5f0dea6c46e2e9c8cd2f7f1681378c9c
lyanecenteno176-lang <lyanecenteno176@gmail.com>
2026-07-29 21:15:49 -0600

    Rectificacion de plantilla Css, integracion de logo .

--- saludya_proyecto/settings.py
+++ saludya_proyecto/settings.py
@@ -96,3 +96,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'saludya_app' / 'static',
 ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
